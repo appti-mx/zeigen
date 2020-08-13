54ea81af906d01467f7964a87b1fccf2682363e3
auralr <41176648+auralr@users.noreply.github.com>
2020-08-12 20:24:05 -0500

    Delete inhered_partner.py

--- apizeigen/models/inhered_partner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models, api
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    phone_related = fields.Char(track_visibility='always', related='phone')
-
