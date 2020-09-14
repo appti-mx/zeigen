21aae88100545b069d8f783d287c7ce96672aafa
auralr <41176648+auralr@users.noreply.github.com>
2020-09-14 12:11:14 -0500

    Update zeigen.py

--- apizeigen/models/zeigen.py
+++ apizeigen/models/zeigen.py
@@ -1974,8 +1974,8 @@ class PurchaseOrder(models.Model):
         order.update({
             'amount_untaxed': order.currency_id.round(subtotal_proveedor_a),
             'amount_tax': order.currency_id.round(amount_tax_a),
-            'amount_total': subtotal_proveedor_a + amount_tax_a,
-            'iva': (subtotal_proveedor_a + amount_tax_a)*.16,
+            'amount_total': subtotal_proveedor_a +((subtotal_proveedor_a)*.16),
+            'iva': (subtotal_proveedor_a)*.16,
         })
 
 
